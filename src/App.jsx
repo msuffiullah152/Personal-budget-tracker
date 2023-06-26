a8a5cd3947ada46fa559ee6d3adcda020e50261a
msuffiullah <112989803+msuffiullah@users.noreply.github.com>
2023-06-26 13:12:58 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TransactionList from './TransactionList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <TransactionList />
+    </div>
+  );
+};
+
+export default App;
