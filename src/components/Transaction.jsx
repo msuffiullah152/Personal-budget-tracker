793599f0653efcc444c92959e4ff6cdc56f2c12c
msuffiullah <112989803+msuffiullah@users.noreply.github.com>
2023-06-27 15:54:39 -0400

    first commit

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './Transaction.css';
 const Transaction = ({ transaction, deleteTransaction }) => {
-  const { id, name, amount } = transaction;
+  const { id, name, amount, date } = transaction;
 
   return (
     <li>
-      <span>{name}</span> ({amount})
+      <span>{name} <br/></span>
+      <span>({amount})<br/></span>
+      <span>{date}<br/></span> 
       <button onClick={() => deleteTransaction(id)}>Delete</button>
     </li>
   );
--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
-import './TransactionForm';
+// import './TransactionForm';
 
 const TransactionForm = ({ addTransaction }) => {
   const [name, setName] = useState('');
   const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
 
-    if (name && amount) {
+    if (name && amount && date) {
       const newTransaction = {
         id: Date.now(),
         name,
         amount: Number(amount),
+        date,
       };
 
       addTransaction(newTransaction);
       setName('');
       setAmount('');
+      setDate('');
     }
   };
 
@@ -34,6 +37,12 @@ const TransactionForm = ({ addTransaction }) => {
         placeholder="Transaction Amount"
         value={amount}
         onChange={e => setAmount(e.target.value)}
+      />
+      <input
+        type="date"
+        placeholder="Transaction Date"
+        value={date}
+        onChange={e => setDate(e.target.value)}
       />
       <button type="submit">Add Transaction</button>
     </form>
